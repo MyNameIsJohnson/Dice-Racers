a534ef3f4298430ef377117847cb8db42e976d74
Johnson Thieu <johnsonle2929@gmail.com>
2020-02-11 14:35:26 -0800

    add eventlisteners

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 // set const for elements 
-const p1 = document.querySelector('.player1');
-const p2 = document.querySelector('.player2');
+const car1 = {
+    score: 0
+};
+const car2 = {
+    score: 0
+};
 
-const d1 = document.querySelector('.d1');
-const d2 = document.querySelector('.d2');
-const d3 = document.querySelector('.d3');
-const d4 = document.querySelector('.d4');
-const d5 = document.querySelector('.d5');
-const d6 = document.querySelector('.d6');
-const d7 = document.querySelector('.d7');
-const d8 = document.querySelector('.d8');
-const raceBtn = document.querySelector('.race');
-const rollBtn = document.querySelector('.roll');
+// cached elements
+let p1 = document.querySelector('.player1');
+let p2 = document.querySelector('.player2');
+let dices = document.querySelectorAll('.dice');
+let d1 = document.querySelector('.d1');
+let d2 = document.querySelector('.d2');
+let d3 = document.querySelector('.d3');
+let d4 = document.querySelector('.d4');
+let d5 = document.querySelector('.d5');
+let d6 = document.querySelector('.d6');
+let d7 = document.querySelector('.d7');
+let d8 = document.querySelector('.d8');
+let carPick1 = document.querySelector('.car1');
+let carPick2 = document.querySelector('.car2');
+let raceBtn = document.querySelector('.race');
+let rollBtn = document.querySelector('.roll');
+
+// Eventlisteners
+car1.addEventListener('click', pickCar);
+car2.addEventListener('click', pickCar);
+raceBtn.addEventListener('click', race);
+rollBtn.addEventListener('click', diceRoll);
+
+// Functions
+
+
 
