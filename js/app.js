08d933a2b3a4f0c5943e7abee604fcb77eb89f9b
Johnson Thieu <johnsonle2929@gmail.com>
2020-02-11 23:03:48 -0800

    added p1 car choice functions

--- js/app.js
+++ js/app.js
@@ -5,13 +5,15 @@ const car1 = {
 const car2 = {
     score: 0
 };
+const btn = document.createElement('button');
+const showCar = document.querySelector('.showCar');
 
 // cached elements
 let p1 = document.querySelector('.player1');
 let p2 = document.querySelector('.player2');
 let pick1Btn = document.querySelector('.car1');
 let pick2Btn = document.querySelector('.car2');
-let carChoice = document.querySelector('.carChoice');
+let carChoice = document.querySelector('.carImages');
 let p1Racer1 = document.querySelector('.racer1');
 let p1Racer2 = document.querySelector('.racer2');
 let p1Racer3 = document.querySelector('.racer3');
@@ -32,25 +34,65 @@ let rollBtn = document.querySelector('.roll');
 
 
 // Eventlisteners
-pick1Btn.addEventListener('click', pickCar);
-pick2Btn.addEventListener('click', pickCar);
-p1Racer1.addEventListener('click', racer1Choice);
-p1Racer2.addEventListener('click', racer1Choice);
-p1Racer3.addEventListener('click', racer1Choice);
-p1Racer4.addEventListener('click', racer1Choice);
+pick1Btn.addEventListener('click', p1PickCar);
+pick2Btn.addEventListener('click', p2PickCar);
+p1Racer1.addEventListener('click', p1racer1Choice, );
+p1Racer2.addEventListener('click', p1racer2Choice);
+p1Racer3.addEventListener('click', p1racer3Choice);
+p1Racer4.addEventListener('click', p1racer4Choice);
 // may have to create duplicate for p2Racer1-4
 
 // raceBtn.addEventListener('click', race);
 // rollBtn.addEventListener('click', diceRoll);
 
-// Functions
-function pickCar(event){
+// Functions    
+function p1PickCar(event){
     carChoice.style.display = 'block';
-// car picked needs to be added to the track
+    showCar.innerHTML = 'Player 1 Choose your Car!'
+
+}
+function p2PickCar(event){
+    carChoice.style.display = 'block';
+    pick1Btn.setAttribute('class', 'carPromt');
 }
 
-function racer1Choice(event){
+// Car picked needs to be added to the track
+// Add p1 car to player1 line
+function p1racer1Choice(event){
+    showCar.innerHTML = '';
     pick1Btn.style.display = 'none';
+    p1.style.backgroundImage = 'url(assets/red.png)';
+    showCar.appendChild(btn).setAttribute('class', 'choice');
+    btn.innerHTML = 'Do you wnat this Car?'; 
+    showCar.style.backgroundImage = 'url(assets/red.png)';
+    showCar.style.display = 'block';
+};
+function p1racer2Choice(event){
+    showCar.innerHTML = '';
+    pick1Btn.style.display = 'none';
+    p1.style.backgroundImage = 'url(assets/yellow.png)';
+    showCar.appendChild(btn).setAttribute('class', 'choice');
+    btn.innerHTML = 'Do you wnat this Car?';
+    showCar.style.backgroundImage = 'url(assets/yellow.png)';
+    showCar.style.display = 'block';
+};
+function p1racer3Choice(event){
+    showCar.innerHTML = '';
+    pick1Btn.style.display = 'none';
+    p1.style.backgroundImage = 'url(assets/blue.png)';
+    showCar.appendChild(btn).setAttribute('class', 'choice');
+    btn.innerHTML = 'Do you wnat this Car?';
+    showCar.style.backgroundImage = 'url(assets/blue.png)';
+    showCar.style.display = 'block';
+};
+function p1racer4Choice(event){
+    showCar.innerHTML = '';
+    pick1Btn.style.display = 'none';
+    p1.style.backgroundImage = 'url(assets/grey.png)';
+    showCar.appendChild(btn).setAttribute('class', 'choice');
+    btn.innerHTML = 'Do you wnat this Car?';
+    showCar.style.backgroundImage = 'url(assets/grey.png)';
+    showCar.style.display = 'block';
+};
 
-}
-
+// add click to display car 
